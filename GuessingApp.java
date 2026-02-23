75e7706bfc1cf8df802ec7ef4ae409b5c412f3bf
anerudh21 <anerudh21@gmail.com>
2026-02-23 12:03:15 +0530

    Added gameCofig and and the GuessingApp file to uc1 feature branch

---
+++ GuessingApp.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+public class GuessingApp {
+	
+	public static void main(String[] args){
+		System.out.println("Welcome to the Guessing App");
+		GameConfig gameConfig = new GameConfig();
+		gameConfig.showRules();
+	}
+}
